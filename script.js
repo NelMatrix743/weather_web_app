3c7ba7014763a6f3bf5baeb8d8df53a2f1f80fb4
NelMatrix743 <nelmatrix155@gmail.com>
2025-09-05 20:49:38 +0000

    feat: retrieve weather information for a city

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-// city name
-let cityName = "abidjan";
+// define utilities
+async function retrieveWeatherInfo(apiKey, cityName){
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&appid=${apiKey}`;
+    const response = await fetch(API_URL);
+    let jsonData = await response.json();
+    //console.log(jsonData);
+    document.querySelector(".city").innerHTML = jsonData.name;
+    document.querySelector(".temp").innerHTML = Math.round(jsonData.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = jsonData.main.humidity + '%';
+    document.querySelector(".wind-speed").innerHTML = jsonData.wind.speed + " km/h";
+}
+
 
 // enter your API KEY here
 const API_KEY = "";
 
-// define API URL
-const API_URL = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&appid=${API_KEY}`;
+const searchButton = document.querySelector(".search button");
+let cityNameInput = document.querySelector(".search input");
 
-async function retrieveWeatherInfo(){
-    const response = await fetch(API_URL);
-    let jsonData = await response.json();
-    console.log(jsonData);
-}
-
-retrieveWeatherInfo();
+// use utilities
+searchButton.addEventListener("click", () => {
+    retrieveWeatherInfo(API_KEY, cityNameInput.value);
+});
